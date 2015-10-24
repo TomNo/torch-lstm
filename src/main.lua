abcbd50516bcbf3ffdd990664204a6280a7d14d9
Tomáš Nováčik <tnovacik@gmail.com>
2015-10-24 11:41:42 +0200

    Add parsing configuration file

--- src/main.lua
+++ src/main.lua
@@ -1,18 +1,18 @@
 require 'torch'
 require 'NeuralNetwork'
 
-
 cmd = torch.CmdLine()
 cmd:text()
 cmd:text()
 cmd:text('Training a simple network')
 cmd:text()
 cmd:text('Options')
-cmd:option('--network','../network.jsn', 'neural network description file')
+cmd:option('--network_file','../network.jsn', 'neural network description file')
+cmd:option('--config_file', '../config.cfg', 'training configuration file')
 cmd:text()
 params = cmd:parse(arg)
 
-net = NeuralNetwork(params.network)
+net = NeuralNetwork(params)
 net:init()
 
 
