b0507a437f8b3cc9ad9a57a842014127168d367a
Tomáš Nováčik <tnovacik@gmail.com>
2015-11-11 13:56:35 +0100

    Added ami script - just test ff

---
+++ ami.lua
@@ -0,0 +1,31 @@
+require 'torch'
+require 'NeuralNetwork'
+require 'Dataset'
+
+cmd = torch.CmdLine()
+cmd:text()
+cmd:text()
+cmd:text('Training a simple network')
+cmd:text()
+cmd:text('Options')
+cmd:option('--network_file','../ami_network.jsn', 'neural network description file')
+cmd:option('--config_file', '../ami_config.cfg', 'training configuration file')
+cmd:option('--log_file', 'ami.log', 'log file')
+cmd:text()
+params = cmd:parse(arg)
+cmd:log(params.log_file, params)
+net = NeuralNetwork(params, cmd)
+net:init()
+train_ds = Dataset(net.conf.train_file)
+tr_data = train_ds:get()
+cv_ds = Dataset(net.conf.val_file)
+cv_data = cv_ds:get()
+test_ds = Dataset(net.conf.test_file)
+test_data = test_ds:get()
+net:train(tr_data, cv_data)
+net:saveModel('ami_network_trained')
+local output = net:forward(test_data)
+output_ds = Dataset('ami_result')
+output_ds:save(output, test_data.tags)
+print("training done")
+
