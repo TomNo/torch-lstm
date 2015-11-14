e9660807b86cfbf2c316fe67b7623bddd011600e
Tomáš Nováčik <tnovacik@gmail.com>
2015-11-14 13:03:42 +0100

    More effective reading of hdf5
    Divided into training dataset - which contains basically only data and testing
    dataset which has tags and is read progressively

--- src/Dataset.lua
+++ src/Dataset.lua
@@ -4,13 +4,87 @@ require 'hdf5'
 torch.setdefaulttensortype('torch.FloatTensor')
 
 local Dataset = torch.class('Dataset')
+local TrainDs, parentTr = torch.class('TrainDs', 'Dataset')
+local TestDs, parentTest = torch.class('TestDs', 'Dataset')
+
 
 function Dataset:__init(filename)
   self.filename = filename
 end
 
--- iterator over hdf5
-function Dataset:dataIter(f)
+-- gets rows and cols of given dataset
+function Dataset:getSize(f, limit)
+--  local iter = self:dataIter(f, limit)
+  local s_rows = false
+  local s_cols = false
+  local rows = 0
+  local cols = 0
+  s_rows, rows = pcall(f.read, f, "/rows")
+  s_cols, cols = pcall(f.read, f, "/cols")
+  rows = rows:all()
+  cols = cols:all()
+  if s_rows and s_cols then
+    if limit and limit < rows[1] then
+      return limit, cols[1]
+    else
+      return rows[1], cols[1]
+    end
+  else
+    error("Could not read size of the dataset.")
+  end
+--  rows = 0
+--  cols = 0
+--  local first = true
+--  while true do
+--    local item = iter()
+--    if not item then
+--      break
+--    end
+--    for _, d_item in pairs(item) do
+--      if first then
+--        first = false
+--        cols = d_item.cols[1]
+--      end
+--      rows = rows + d_item.rows[1]
+--      if limit and rows > limit then
+--        return limit, cols
+--      end    
+--    end
+--  end
+--  return rows, cols
+end
+
+function TrainDs:__init(filename)
+  parentTr.__init(self, filename)
+end
+
+function TrainDs:get(limit)
+    if limit then
+    print("Loading training dataset with limit " .. limit .. " from file " .. self.filename)
+  else
+    print("Loading training dataset from file " .. self.filename)
+  end
+  local f = hdf5.open(self.filename)
+  local o_rows = 0
+  local o_cols = 0
+  o_rows, o_cols = self:getSize(f, limit)
+  local labels = f:read("labels"):partial({1, o_rows})
+  local features = f:read("features"):partial({1, o_rows}, {1, o_cols})
+  local o_data = {[1]=features, [2]=labels}
+  o_data.size = function() return o_rows end
+  o_data.rows = o_data.size 
+  o_data.cols = function() return o_cols end
+  print("Dataset loaded.")
+  f:close()
+  return o_data
+end
+
+function TestDs:__init(filename)
+  parentTest.__init(self, filename)
+end
+
+-- iterator over test structure of hdf5
+function TestDs:dataIter(f)
   local counter = 0
   return function()
            local status, data = pcall(f.read, f, "/" .. counter)
@@ -21,75 +95,18 @@ function Dataset:dataIter(f)
          end
 end
 
--- goes through the whole dataset(if limit not specified) and gather number of rows and columns
-function Dataset:getSize(f, limit)
-  local iter = self:dataIter(f, limit)
-  local s_rows = false
-  local s_cols = false
-  local rows = 0
-  local cols = 0
-  s_rows, rows = pcall(f.read, "/rows")
-  s_cols, cols = pcall(f.read, "/cols")
-  if s_rows and s_cols then
-    if limit and limit < rows[1] then
-      return limit, cols[1]
-    else
-      return rows[1], cols[1]
-    end
-  end
-  local first = true
-  while true do
-    local item = iter()
-    if not item then
-      break
-    end
-    for _, d_item in pairs(item) do
-      if first then
-        first = false
-        cols = d_item.cols[1]
-      end
-      rows = rows + d_item.rows[1]
-      if limit and rows > limit then
-        return limit, cols
-      end    
-    end
-  end
-  return rows, cols
-end
-
--- Checks wheather dataset cotains labels
-function Dataset:labelsPresent(f)
-  if self.l_present ~= nil then
-    return self.l_present
-  end
-  local iter = self:dataIter(f)
-  for _, item in pairs(iter()) do
-    if item.labels then
-      self.l_present = true
-    else
-      self.l_present = false
-    end
-    break
-  end
-  return self.l_present
-end
-
 -- reads the data from input file and returns the dataset
-function Dataset:get(limit)
+function TestDs:get(limit)
   if limit then
-    print("Loading dataset with limit " .. limit .. " from file " .. self.filename)
+    print("Loading test dataset with limit " .. limit .. " from file " .. self.filename)
   else
-    print("Loading dataset from file " .. self.filename)
+    print("Loading test dataset from file " .. self.filename)
   end
   local f = hdf5.open(self.filename)
   local o_rows = 0
   local o_cols = 0
   o_rows, o_cols = self:getSize(f, limit)
   local features = torch.Tensor(o_rows, o_cols)
-  local labels = nil
-  if self:labelsPresent(f) then
-    labels = torch.Tensor(o_rows)  
-  end
   local tags = {}
   local size = 0
   local l_reached = false
@@ -113,15 +130,13 @@ function Dataset:get(limit)
         size = size + 1
         features[size] = item.data[{{i*cols + 1, (i+1)*cols}}]
       end
-      if self:labelsPresent(f) then
-        -- numbersing in lua starts from 1...
-        labels[{{size - rows + 1, size}}] = item.labels[{{1, rows}}]:add(1)
-      end
       table.insert(tags, {tag, rows})
     end      
   end 
-  local o_data = {[1]=features, [2]=labels}
+  local o_data = {[1]=features}
   o_data.size = function() return o_rows end
+  o_data.rows = o_data.size 
+  o_data.cols = function() return o_cols end
   o_data.tags = tags
   print("Dataset loaded.")
   f:close()
@@ -129,7 +144,7 @@ function Dataset:get(limit)
 end
 
 -- saves the data into output file specified on initialization
-function Dataset:save(data, tags)
+function TestDs:save(data, tags)
   print("Saving dataset into the file " .. self.filename)
   local f = hdf5.open(self.filename, "w")
   local counter = 1
@@ -141,7 +156,7 @@ function Dataset:save(data, tags)
     rows[1] = t_data:size(1)
     local cols = torch.Tensor(1)
     cols[1] = t_data:size(2)
-    f:write(tag .. "/data", t_data:view(t_data:nElement()))
+    f:write(tag .. "/data", t_data)
     f:write(tag .. "/rows", rows)
     f:write(tag .. "/cols", cols)
     counter = counter + size
@@ -149,3 +164,25 @@ function Dataset:save(data, tags)
   f:close()
   print("Dataset saved.")
 end
+
+
+
+-- Checks wheather dataset cotains labels
+--function Dataset:labelsPresent(f)
+--  if self.l_present ~= nil then
+--    return self.l_present
+--  end
+--  local iter = self:dataIter(f)
+--  for _, item in pairs(iter()) do
+--    if item.labels then
+--      self.l_present = true
+--    else
+--      self.l_present = false
+--    end
+--    break
+--  end
+--  return self.l_present
+--end
+
+
+
