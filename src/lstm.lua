231ca76217844a8908462a4dea7d3ae3ae4548f4
Tomáš Nováčik <tnovacik@gmail.com>
2016-01-02 12:10:43 +0100

    fix gradient propagation from p. timestep

--- src/lstm.lua
+++ src/lstm.lua
@@ -6,6 +6,7 @@ local Lstm, parent = torch.class('Lstm', 'nn.Container')
 
 --TODO there is no support for non batch mode
 --TODO resizing array should be optimized
+--TODO biases arent correct in gates
 
 function Lstm:__init(inputSize, layerSize, hist)
   parent.__init(self)
@@ -83,24 +84,33 @@ function Lstm:__init(inputSize, layerSize, hist)
   -- copies of 'step' module
   table.insert(self.modules, self.model)
   for i=2, hist do
-    table.insert(self.modules, self.model:clone('weight','bias'))
+    table.insert(self.modules, self.model:clone('weight','bias','gradWeight','gradBias'))
   end
 end
 
 function Lstm:updateGradInput(input, gradOutput)
   self.g_output:resize(self.batch_size * self.history_size, self.a_count)
+  local size = gradOutput:size(1)
+  local interval = {size - self.batch_size + 1, size} 
   local z_tensor = self.z_tensor:repeatTensor(self.batch_size, 1)
-  self.model:backward({{self.a_i_acts[{{1, self.batch_size}, {}}], z_tensor}, z_tensor},
-  gradOutput[{{1,self.batch_size}, {}}])
-  self.g_output[{{1, self.batch_size}, {}}]:copy(self.model.gradInput[1][1])
-  for i=3, #self.modules do
-    local step = self.modules[i]
-    local p_step = self.modules[i-1]
-    local s = (i-2)*self.batch_size + 1
-    local e = (i-1)*self.batch_size
-    local inp = {{self.a_i_acts[{{s,e},{}}], p_step.output}, self:getCellStates(p_step)}
-    step:backward(inp, gradOutput[{{s,e}, {}}])
-    self.g_output[{{s,e}, {}}]:copy(step.gradInput[1][1])
+  local inp = {{self.a_i_acts[{interval, {}}], z_tensor}, z_tensor}
+  local tmp_g = gradOutput[{interval, {}}]
+  -- first do propagation from the last module
+  local l_step = self.modules[#self.modules]
+  l_step:backward(inp, tmp_g)
+  self.g_output[{interval, {}}]:copy(l_step.gradInput[1][1]) -- error for the next layer
+  local counter = 2
+  for i=1, #self.modules -2 do
+    local c = #self.modules - i
+    local step = self.modules[c]
+    local p_step = self.modules[c+1]
+    interval = {size - counter * self.batch_size +1, size - (counter -1) * self.batch_size}
+    local inp = {{self.a_i_acts[{interval,{}}], p_step.output}, self:getCellStates(p_step)}
+    local i_grad = gradOutput[{interval, {}}]:add(p_step.gradInput[1][2]) -- propagate error from previous time step
+    p_step.gradInput[1][2]:zero() -- gradient accumulation should not happen in here
+    step:backward(inp, i_grad) 
+    self.g_output[{interval, {}}]:copy(step.gradInput[1][1])
+    counter = counter + 1
   end
   self.gradInput = self.a_i_acts_module:backward(input, self.g_output)
   return self.gradInput
@@ -108,21 +118,32 @@ end
 
 function Lstm:updateOutput(input)
   -- TODO this resizing might be handled better
-  self.batch_size = input:size(1) / self.history_size
-  self.output:resize(self.history_size * self.batch_size, self.layerSize)
-  self.a_i_acts = self.a_i_acts_module:forward(input)
-  local z_tensor = self.z_tensor:repeatTensor(self.batch_size, 1)
-  -- do first step manually, set previous output and previous cell state to zeros
-  self.model:forward({{self.a_i_acts[{{1, self.batch_size}, {}}], z_tensor}, z_tensor})
-  self.output[{{1, self.batch_size}, {}}]:copy(self.model.output)
-  for i= 3, #self.modules do
-    local p_step = self.modules[i-1]
-    local step = self.modules[i]
-    local s = (i-2)*self.batch_size + 1
-    local e = (i-1)*self.batch_size
-    local t_i_acts = self.a_i_acts[{{s, e},{}}]
-    step:forward({{t_i_acts, p_step.output}, self:getCellStates(p_step)})
-    self.output[{{s,e},{}}]:copy(step.output)
+  if not self.train then -- threat input as one long sequence
+    self.output:resize(input:size(1), self.layerSize)
+    self.a_i_acts = self.a_i_acts_module:forward(input)
+    self.model:forward({{self.a_i_acts[1], self.z_tensor[1]}, self.z_tensor[1]})
+    self.output[1] = self.model.output
+    for i=2, input:size(1) do
+      self.model:forward({{self.a_i_acts[i], self.model.output}, self:getCellStates(self.model)})
+      self.output[i] = self.model.output
+    end
+    return self.output
+  else -- training mode
+    self.batch_size = input:size(1) / self.history_size
+    self.output:resize(self.history_size * self.batch_size, self.layerSize)
+    self.a_i_acts = self.a_i_acts_module:forward(input)
+    local z_tensor = self.z_tensor:repeatTensor(self.batch_size, 1)
+    -- do first step manually, set previous output and previous cell state to zeros
+    self.model:forward({{self.a_i_acts[{{1, self.batch_size}, {}}], z_tensor}, z_tensor})
+    self.output[{{1, self.batch_size}, {}}]:copy(self.model.output)
+    for i= 3, #self.modules do
+      local p_step = self.modules[i-1]
+      local step = self.modules[i]
+      local interval = {(i-2)*self.batch_size + 1, (i-1)*self.batch_size}
+      local t_i_acts = self.a_i_acts[{interval,{}}]
+      step:forward({{t_i_acts, p_step.output}, self:getCellStates(p_step)})
+      self.output[{interval,{}}]:copy(step.output)
+    end
   end
   return self.output
 end
@@ -137,11 +158,17 @@ function Lstm:__tostring__()
       string.format('(%d -> %d)', self.inputSize, self.layerSize)
 end
 
+--a = Lstm.new(10, 20, 50)
+--inp = torch.randn(16*50,10)
+--a:training() --evaluate()
+--_,b = a:getParameters()
+--output = a(inp)
+--
+--a:backward(inp, torch.randn(16*50, 20))
+--a:backward(inp, torch.randn(16*50, 20))
+--a:backward(inp, torch.randn(16*50, 20))
+--a:backward(inp, torch.randn(16*50, 20))
 return Lstm
 
---a = Lstm.new(10, 20, 50)
---print(a.model)
---inp = torch.randn(16*50,10)
---output = a(inp)
---a:backward(inp, torch.randn(16*50, 20))
+
 
