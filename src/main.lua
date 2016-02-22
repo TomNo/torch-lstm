ef900f2309fc81d2ef92be81543e868f14981d16
Tomáš Nováčik <tnovacik@gmail.com>
2016-02-22 22:57:35 +0100

    add model as cmd line para

--- src/main.lua
+++ src/main.lua
@@ -15,10 +15,14 @@ cmd:option('--forward_output', '', 'resulting forward pass output file')
 cmd:option('--config_file', '../timit_config.cfg', 'training configuration file')
 cmd:option('--log_file', 'timit.log', 'log file')
 cmd:option('--output_model', 'final.model', 'name of the resulting serialized model')
+cmd:option('--input_model', '', 'name of the resulting serialized model')
 cmd:text()
 params = cmd:parse(arg)
 cmd:log(params.log_file, params)
 net = NeuralNetwork(params, cmd)
+if params.input_model ~= '' then
+    net.conf.model = params.input_model
+end
 net:init()
 
 if params.forward_pass then
