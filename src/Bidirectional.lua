93ab523aa745d94c65a0ca8fcbcb3f633492b365
Tomáš Nováčik <tnovacik@gmail.com>
2016-03-28 23:05:17 +0200

    bidirectional - remove split

--- src/Bidirectional.lua
+++ src/Bidirectional.lua
@@ -17,20 +17,26 @@ function Bidirectional:__init(inputSize, layerSize, hist, bNorm)
     self.inputSize = inputSize
     self.history = hist
     self:_setActualModule()
-    self:add(nn.Linear(inputSize, self.aModule.inputSize * 2, false))
+    self.fModule = nn.Sequential()
+    self.fModule:add(nn.Linear(inputSize, self.aModule.inputSize, false))
     if bNorm then
-        self:add(nn.BatchNormalization(self.aModule.inputSize *2))
+        self.fModule:add(nn.BatchNormalization(self.aModule.inputSize))
     end
-    self:add(nn.Split())
-    self.fModule = self.aModule
+    self.fModule:add(self.aModule)
+
     self.bModule = nn.Sequential()
+    self.bModule:add(nn.Linear(inputSize, self.aModule.inputSize, false))
+    if bNorm then
+        self.bModule:add(nn.BatchNormalization(self.aModule.inputSize))
+    end
     self.bModule:add(nn.Revert())
     self.bModule:add(self.aModule:clone())
     self.bModule:add(nn.Revert())
-    local pTable = nn.ParallelTable()
-    pTable:add(self.fModule)
-    pTable:add(self.bModule)
-    self:add(pTable)
+
+    local concat = nn.ConcatTable()
+    concat:add(self.fModule)
+    concat:add(self.bModule)
+    self:add(concat)
     self:add(nn.JoinTable(2, 2))
 end
 
