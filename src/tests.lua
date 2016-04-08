81c1b5788adeac74eeeaa596801c132b683e6df6
Tomáš Nováčik <tnovacik@gmail.com>
2016-04-08 12:51:28 +0200

    test refactoring

--- src/tests.lua
+++ src/tests.lua
@@ -1,6 +1,5 @@
 require 'torch'
 require 'nn'
-require 'rnn'
 require 'cutorch'
 require 'cunn'
 require 'ParallelTable'
@@ -8,6 +7,7 @@ require 'ParallelTable'
 torch.class("nn.RegularTanh", "nn.Tanh")
 
 --TODO generalize - to much code duplication :D!!!
+-- TODO bidirectional and simple rnn
 
 tester = torch.Tester()
 
@@ -15,7 +15,7 @@ tester = torch.Tester()
 classNames = { "LinearScale", "LstmStep", "Lstm", "Blstm", "GruStep", "Gru", "Bgru", "RecLayer" }
 
 
-cond = 1e-10
+cond = 1e-4
 
 function testClass(class)
     local lSize = 6
@@ -59,90 +59,14 @@ for i = 1, #classes do
     tester:add(testFunction, "BasicTest" .. classNames[i])
 end
 
-LstmTest = torch.TestSuite()
 
-
-function LstmTest.LstmBatched()
-    local iSize = 3
-    local oSize = 3
-    local hSize = 3
-    local bSize = 2
-    local b = nn.Lstm(iSize, oSize, hSize)
-    local a = nn.Lstm(iSize, oSize, hSize)
-    local x_b, _ = b:getParameters()
-    local x_a, _ = a:getParameters()
-    x_b:copy(x_a)
-    local inp = torch.ones(hSize, iSize)
-    local result = b:forward(inp):clone()
-    local errs = b:backward(inp, torch.range(1, oSize):repeatTensor(hSize, 1))
-    local e_errs = torch.Tensor(bSize * hSize, oSize)
-    local e_result = e_errs:clone()
-    local y = 1
-    for i = 1, bSize * hSize, bSize do
-        e_result[i]:copy(result[y])
-        e_result[i + 1]:copy(result[y])
-        e_errs[i]:copy(errs[y])
-        e_errs[i + 1]:copy(errs[y])
-        y = y + 1
-    end
-    local batched_inp = torch.ones(bSize * hSize, 3)
-    local a_result = a:forward(batched_inp)
-    local a_errs = a:backward(batched_inp, torch.range(1, 3):repeatTensor(6, 1))
-    if not torch.all(torch.eq(a_errs, e_errs)) then
-        print(a_errs)
-        print(e_errs)
-        print("Error: Backward pass does not match.")
-    end
-    if not torch.all(torch.eq(a_result, e_result)) then
-        print(a_result)
-        print(e_result)
-        print("Error: Forward pass does not match.")
-    end
-end
-
-function LstmTest.LstmForwardBackward()
-    local w_const = 0.3
-    local history = 4
-    local a = nn.Lstm(1, 1, history)
-    local b = nn.LSTM(1, 1)
-    local a_params = a:getParameters()
-    local b_params = b:getParameters()
-    a_params:fill(w_const)
-    b_params:fill(w_const)
-    local i_a = torch.ones(history, 1)
-    local i_b = torch.ones(1)
-    local o_a = a:forward(i_a)
-    local f_output = {}
-    for i = 1, history do
-        f_output[i] = b:forward(i_b)
-    end
-    local o_b = torch.cat(f_output):view(history, 1)
-    local e_a = a:backward(i_a, torch.ones(history, 1))
-    local b_output = {}
-    for i = 1, history do
-        b_output[i] = b:backward(i_b, torch.ones(1))
-    end
-    local e_b = torch.cat(b_output):view(history, 1)
-    tester:assertTensorEq(o_a, o_b, cond, "Outputs do not match")
-
-    for i = 1, history do
-        tester:assertTensorEq(e_a[i], e_b[history + 1 - i], cond,
-            "Backward errors do not match.")
-    end
-
-    return 0
-end
-
-
-BlstmTests = torch.TestSuite()
-
-function BlstmTests:BlstmBatched()
+function testBatch(module)
     local iSize = 3
     local oSize = 6
     local hSize = 3
     local bSize = 2
-    local b = nn.Blstm(iSize, oSize, hSize)
-    local a = nn.Blstm(iSize, oSize, hSize)
+    local b = module(iSize, oSize, hSize)
+    local a = module(iSize, oSize, hSize)
     local x_b, _ = b:getParameters()
     local x_a, _ = a:getParameters()
     x_b:copy(x_a)
@@ -161,91 +85,80 @@ function BlstmTests:BlstmBatched()
     end
     local batched_inp = torch.ones(bSize * hSize, iSize)
     local a_result = a:forward(batched_inp)
-    local a_errs = a:backward(batched_inp, torch.range(1, oSize):repeatTensor(6, 1))
-    if not torch.all(torch.eq(a_errs, e_errs)) then
-        print(a_errs)
-        print(e_errs)
-        print("Error: Backward pass does not match.")
-    end
-    if not torch.all(torch.eq(a_result, e_result)) then
-        print(a_result)
-        print(e_result)
-        print("Error: Forward pass does not match.")
-    end
+    local a_errs = a:backward(batched_inp, torch.range(1, oSize):repeatTensor(oSize, 1))
+    tester:assertTensorEq(a_result, e_result, cond, "Forward pass do not match.")
+    tester:assertTensorEq(a_errs, e_errs, cond, "Backward pass does not match.")
 end
+
+local batchModules = { nn.Bgru, nn.Blstm, nn.Gru, nn.Lstm}
+local batchNames = { "Bgru", "Blstm", "Lstm", "Gru", "Bgru", "Blstm"}
+
+for i = 1, #batchModules do
+    local testFunction = function()
+        testBatch(batchModules[i])
+    end
+    tester:add(testFunction, "TestBatched".. batchNames[i])
+end
+
+
+--function testBidirectional(bModule, uModule)
+--    local iSize = 2
+--    local oSize = 4
+--    local hSize = 3
+--    local input = torch.ones(hSize, iSize)
+--    local bModel = bModule(iSize, oSize, hSize)
+--    print(bModel)
+--    local uModel = uModule(iSize, oSize/2, hSize)
+--    print(input)
+--    local uOutput =uModule:forward(input)
+--    print(input)
+--    local bOutput = bModule:forward(input)
+--    tester:assertTensorEq(bOutput[{{1, oSize/2}}], uOutput, "Outputs do not match.")
+--    tester:assertTensorEq(bOutput[{{oSize/2 + 1, oSize}}], uOutput, "Outputs do not match.")
+--
+--end
+--
+--
+--local biModules = { nn.Bgru, nn.Blstm}
+--local uModules = {nn.Gru, nn.Lstm}
+--local biNames = { "Gru", "Lstm"}
+--
+--for i = 1, #biModules do
+--    local testFunction = function()
+--        testBidirectional(biModules[i], uModules[i])
+--    end
+--    tester:add(testFunction, "TestBidirectional".. biNames[i])
+--end
+
+LstmTest = torch.TestSuite()
+
+
+function testForwardBachward(module, e_output, e_error)
+    local w_const = 0.3
+    local history = 4
+    local obj = module(1, 1, history)
+    local params = obj:getParameters()
+    params:fill(w_const)
+    local input = torch.ones(history, 1)
+    local output = obj:forward(input)
+    local error = obj:backward(input, torch.ones(history, 1)):squeeze()
+    tester:assertTensorEq(output:squeeze(), e_output, cond, "Outputs do not match.")
+    tester:assertTensorEq(error, e_error, cond, "Errors do not match.")
+end
+
+function LstmTest.LstmForwardBackward()
+    local e_output = torch.Tensor({0.2231, 0.3946, 0.5189, 0.6064})
+    local e_error = torch.Tensor({0.3165, 0.2701, 0.1930, 0.1125})
+    testForwardBachward(nn.Lstm, e_output, e_error)
+end
+
 
 GruTest = torch.TestSuite()
 
-function GruTest:GruBatched()
-    local iSize = 3
-    local oSize = 3
-    local hSize = 3
-    local bSize = 2
-    local b = nn.Gru(iSize, oSize, hSize)
-    local a = nn.Gru(iSize, oSize, hSize)
-    local x_b, _ = b:getParameters()
-    local x_a, _ = a:getParameters()
-    x_b:copy(x_a)
-    local inp = torch.ones(hSize, iSize)
-    local result = b:forward(inp):clone()
-    local errs = b:backward(inp, torch.range(1, oSize):repeatTensor(hSize, 1))
-    local e_errs = torch.Tensor(bSize * hSize, oSize)
-    local e_result = e_errs:clone()
-    local y = 1
-    for i = 1, bSize * hSize, bSize do
-        e_result[i]:copy(result[y])
-        e_result[i + 1]:copy(result[y])
-        e_errs[i]:copy(errs[y])
-        e_errs[i + 1]:copy(errs[y])
-        y = y + 1
-    end
-    local batched_inp = torch.ones(bSize * hSize, 3)
-    local a_result = a:forward(batched_inp)
-    local a_errs = a:backward(batched_inp, torch.range(1, 3):repeatTensor(6, 1))
-    if not torch.all(torch.eq(a_errs, e_errs)) then
-        print(a_errs)
-        print(e_errs)
-        print("Error: Backward pass does not match.")
-    end
-    if not torch.all(torch.eq(a_result, e_result)) then
-        print(a_result)
-        print(e_result)
-        print("Error: Forward pass does not match.")
-    end
-end
-
-
 function GruTest:GruForwardBackward()
-    local w_const = 0.3
-    local history = 4
-    local a = nn.Gru(1, 1, history)
-    local b = nn.GRU(1, 1)
-    local a_params = a:getParameters()
-    local b_params = b:getParameters()
-    a_params:fill(w_const)
-    b_params:fill(w_const)
-    local i_a = torch.ones(history, 1)
-    local i_b = torch.ones(1)
-    local o_a = a:forward(i_a)
-    local f_output = {}
-    for i = 1, history do
-        f_output[i] = b:forward(i_b)
-    end
-    local o_b = torch.cat(f_output):view(history, 1)
-    local e_a = a:backward(i_a, torch.ones(history, 1))
-    local b_output = {}
-    for i = 1, history do
-        b_output[i] = b:backward(i_b, torch.ones(1))
-    end
-    local e_b = torch.cat(b_output):view(history, 1)
-    tester:assertTensorEq(o_a, o_b, cond, "Outputs do not match")
-
-    for i = 1, history do
-        tester:assertTensorEq(e_a[i], e_b[history + 1 - i], cond,
-            "Backward errors do not match.")
-    end
-
-    return 0
+    local e_error = torch.Tensor({0.096448, 0.099792, 0.085345, 0.052972})
+    local e_output = torch.Tensor({0.1903, 0.3176,0.4052,0.4665})
+    testForwardBachward(nn.Gru, e_output, e_error)
 end
 
 function testCtc()
@@ -274,7 +187,6 @@ end
 
 
 tester:add(LstmTest)
-tester:add(BlstmTests)
 tester:add(GruTest)
 tester:add(testCtc, "CtcForwardBackward")
 
