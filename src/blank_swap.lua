2ec82f87ebd16f82012516e74ad205534ee3fa1b
Tomáš Nováčik <tnovacik@gmail.com>
2016-05-23 18:39:01 +0200

    blank transfer script added

--- src/blank_swap.lua
+++ src/blank_swap.lua
@@ -1,8 +1,27 @@
---
--- Created by IntelliJ IDEA.
--- User: tom
--- Date: 23.5.16
--- Time: 14:02
--- To change this template use File | Settings | File Templates.
---
+require 'hdf5'
 
+-- swaps the blank symbol from 1st position to the last in the *hdf5*
+-- file format because essen assumes its position differently
+
+cmd = torch.CmdLine()
+cmd:text()
+cmd:text()
+cmd:text('Swaps the position of the blank symbol in the input file.')
+cmd:option('--input_file', 'input.hdf5', 'Input file')
+cmd:option('--output_file', 'output_swapped.hdf5', 'Output file with swapped blank')
+
+params = cmd:parse(arg)
+
+local iFile = hdf5.open(params.input_file)
+local iData = iFile:read():all()
+for _, val in pairs(iData) do
+    local size = val["data"]:size(2)
+    local a1 = val["data"][{{},{1, 1}}]:clone()
+    local a2 = val["data"][{{}, {size, size}}]:clone()
+  val["data"][{{}, {1,1}}] = a2
+  val["data"][{{}, {size, size}}] = a1
+end
+
+local oFile = hdf5.open(params.output_file, "w")
+oFile:write("/", iData)
+oFile:close()
