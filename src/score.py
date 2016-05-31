b87ee148f79f6957303ba27c6927715e7678815d
Tomáš Nováčik <tnovacik@gmail.com>
2016-05-31 21:53:04 +0200

    last

---
+++ src/score.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+__author__ = 'Tomas Novacik'
+
+
+class(object):
+    """ """
+    # constants
+
+    # private methods
+
+    # public methods
+
+    # protected methods
+
+    # class methods
+
+    # static methods
+
+# eof
